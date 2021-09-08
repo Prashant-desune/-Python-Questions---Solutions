c5faa947d3804b11c601ef6d7471333855028ac9
Prashant Kumar <prashant.desune@gmail.com>
2021-09-08 12:35:43 +0530

    Add files via upload

---
+++ Question7-Python Program to Convert Kilometer to Miles.py
@@ -0,0 +1,5 @@
+#Q.7)Python Program to Convert Kilometers to Miles
+kilo = int(input("Kilometer::"))
+conver_fraction = 0.621371
+Miles = kilo * conver_fraction
+print("after Convert Kilometers to Miles is:%0.2f" %Miles)
