f8f4b68d5a8e78220e953e272aacc5d0f08f0083
Prashant Kumar <prashant.desune@gmail.com>
2021-09-10 23:32:59 +0530

    Add files via upload

---
+++ Question15-Python Program to Print all Prime Numbers in an Interval.py
@@ -0,0 +1,10 @@
+#Q.14)Python Program to Print all Prime Numbers in an Interval
+lower = int(input("1::"))
+upper = int(input("2::"))
+for num in range(lower, upper+1):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
